58c9fe0573128a9e9ecb0b6d1f7f238dd07b9410
yonyong <2365878736@qq.com>
2019-01-12 12:54:05 +0800

    Merge branch 'master' of https://github.com/njkernel/OMS4Intern
    # Conflicts:
    #	src/main/java/cn/com/connext/oms/commons/utils/ToMap.java
    #	src/main/java/cn/com/connext/oms/entity/TbOrderDetails.java
    #	src/main/java/cn/com/connext/oms/mapper/TbOutputMapper.java
    #	src/main/java/cn/com/connext/oms/service/impl/OutputServiceImpl.java
    #	src/main/java/cn/com/connext/oms/web/Controller/OutputController.java
    #	src/main/resources/mapper/TbOutputMapper.xml

--- src/main/java/cn/com/connext/oms/web/Api/OutputApi.java
+++ src/main/java/cn/com/connext/oms/web/Api/OutputApi.java
@@ -1,64 +1,69 @@
 package cn.com.connext.oms.web.Api;
 
-import cn.com.connext.oms.commons.dto.OutRepoOrderDetailDto;
-import cn.com.connext.oms.commons.dto.OutputDTO;
-import cn.com.connext.oms.entity.TbOrder;
-import cn.com.connext.oms.entity.TbOutput;
-import cn.com.connext.oms.entity.TbReceiver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.client.RestTemplate;
-import java.util.List;
+import cn.com.connext.oms.commons.utils.HttpClientUtils.HttpClientUtil;
+import cn.com.connext.oms.commons.utils.HttpClientUtils.common.HttpConfig;
+import cn.com.connext.oms.commons.utils.HttpClientUtils.exception.HttpProcessException;
 
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * <p>Title: OutputApi</p>
- * <p>Description: 出库模块，推送出库单 </p>
- *
- * @author Jay
- * @version 1.0.0
- * @Date 2019/1/7
- */
-public class OutputApi {
-    public static String post(TbOutput tbOutput, TbOrder tbOrder, TbReceiver tbReceiver, List<OutRepoOrderDetailDto> repoOrderDetailDto) throws Exception {
-        OutputDTO outputDTO=new OutputDTO(
-                tbOutput.getOutputCode(),
-                tbOutput.getOrderId(),
-                tbOrder.getChannelCode(),
-                tbReceiver.getReceiverName(),
-                tbReceiver.getReceiverAddress(),
-                tbOrder.getDeliveryCompany(),
-                repoOrderDetailDto
-        );
-        String result= new RestTemplate().postForObject("http://10.129.100.131:8080/api/pushOutRepoOrder",outputDTO.toMap(),String.class);
-        return result;
-    }
-    }
-//        Map<String,Object> map=new HashMap<String, Object>();
-//        // 存入所需要的字段和数据
-//        map.put("outRepoId",tbOutput.getOutputCode());
-//        map.put("orderId",tbOutput.getOrderId());
-//        map.put("channelId",tbOrder.getChannelCode());
-//        map.put("receiverName",tbReceiver.getReceiverName());
-//        map.put("receiverAddress",tbReceiver.getReceiverAddress());
-//        map.put("expressCompany",tbOrder.getDeliveryCompany());
-//        map.put("outRepoOrderDetailDto",);
-//        // 传送是否成功接收返回值
-//        String s = HttpClientUtil.post(HttpConfig.custom().client(),
-//                 // 连接到WMS的地址er
-//                "http://10.129.100.131:8080/api/pushOutRepoOrder",
-//                HttpConfig.custom().headers(),
-//                map, HttpConfig.custom().context(),
-//                HttpConfig.custom().encoding());
-//        return s;
-//   }
-//
-//    /**
-//     * 调用WMS接口，获取返回值
-//     * @return
-//     * @throws Exception
-//     */
-//    public static int isReceived() throws Exception {
-//        int t = HttpClientUtil.get(HttpConfig.custom().url("http://localhost:8502/"));
-//        return t;
-//    }
+ * @program: OMS4Intern
+ * @description: 调用WMS有关出库单的接口
+ * @author: Lili.Chen
+ * @create: 2019-01-09 12:48
+ **/
+public class OutputAPI {
 
+    /**
+    * @Description: 从wms获取出库单的状态（用出库单编码）
+    * @Param: [outputCode]
+    * @return: java.lang.String
+    * @Author: Lili Chen
+    * @Date: 2019/1/9
+    */
+    public static String getOutPutState(String outputCode) throws HttpProcessException {
+        Map<String,Object> map=new HashMap<String, Object>();
+        map.put("outRepoOrderNo",outputCode);
+        String s = HttpClientUtil.post(HttpConfig.custom().client(),
+                "http://10.129.100.39:8080/api/getOutRepoOrderStatus",
+                HttpConfig.custom().headers(),
+                map, HttpConfig.custom().context(),
+                HttpConfig.custom().encoding());
+        return s;
+    }
+
+
+
+    public static String updateOutputStateOfWMS(String outputCodeArray) throws HttpProcessException {
+        Map<String,Object> map=new HashMap<String, Object>();
+        map.put("outRepoOrderNo",outputCodeArray);
+        String s = HttpClientUtil.post(HttpConfig.custom().client(),
+                "http://10.129.100.39:8080/api/cancelResult",
+                HttpConfig.custom().headers(),
+                map, HttpConfig.custom().context(),
+                HttpConfig.custom().encoding());
+        return s;
+    }
+
+
+
+    public static String post() throws HttpProcessException {
+        Map<String,Object> map=new HashMap<String, Object>();
+        String[] a=new String[2];
+        a[0]=1+"";
+        a[1]=2+"";
+      /*  String a="1,2";*/
+        for(String s:a){
+            System.out.println(s);
+        }
+
+        map.put("outRepoOrderIdArray",a);
+        String s = HttpClientUtil.post(HttpConfig.custom().client(),
+                "http://10.129.100.39:8080/api/cancelResult",
+                HttpConfig.custom().headers(),
+                map, HttpConfig.custom().context(),
+                HttpConfig.custom().encoding());
+        return s;
+    }
+}
