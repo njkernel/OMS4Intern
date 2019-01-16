e2da85657ee8300edaf8ca21ad68ab8db6504224
faramita <279205343@qq.com>
2019-01-16 16:02:40 +0800

    订单复选框 Signed-off-by: faramita <279205343@qq.com>

--- src/main/resources/static/page/js/orderList.js
+++ src/main/resources/static/page/js/orderList.js
@@ -7,9 +7,10 @@ let orderList = new Vue({
                 pageSize:5,
                 pageNum:1,
             },
+            orderId:"",
             orderListDate:[],
             //选中行的记录id
-            checkedDate:"",
+            checkedNames:[],
             //搜索输入框
             searchInput:"",
             //异常单数据
@@ -73,7 +74,7 @@ let orderList = new Vue({
         //异常处理
         abnormalHandle(){
             let url='/abnormalHandle';
-            callAxiosGet(url,{abnormalId:this.checkedDate},this.Suc,this.Fail)
+            callAxiosGet(url,{abnormalId:this.checkedNames[0]},this.Suc,this.Fail)
         },
         //把当前行id存在缓存中
             toPageStorage(){
@@ -81,10 +82,11 @@ let orderList = new Vue({
                  /!*console.log(sessionStorage.getItem("id"))*!/
              },
 
-        //异常订单详情
+        //订单详情
         orderDetails(){
-            console.log(this.checkedDate);
-            document.getElementById('iframeId3').src="/orderDetail?orderId="+this.checkedDate;
+            this.orderId=this.checkedNames[0];
+            console.log(this.orderId);
+            document.getElementById('iframeId3').src="/orderDetail?orderId="+this.orderId;
         },
 
 
@@ -154,7 +156,7 @@ let orderList = new Vue({
                 that.orderListDate=res.data;
                 console.log(that.orderListDate);
                 //默认选中第一条数据
-                that.checkedDate=res.data.list[0].abnormalId;
+                that.checkedDate[0]=res.data.list[0].abnormalId;
 
             }else{
                 alert(res.message);
@@ -169,10 +171,11 @@ let orderList = new Vue({
         },
         //接口未连通
         Fail(err){
-            console.log("网络连接错误")
+
         },
         orderCheck(){
-            let url='/orderCheck?orderId='+this.checkedDate;
+            this.orderId=this.checkedNames[0];
+            let url='/orderCheck?orderId='+this.orderId;
             callAxiosGetNoParam(url,this.orderCheckSuc,this.orderCheckFail);
         },
         orderCheckSuc(res){
