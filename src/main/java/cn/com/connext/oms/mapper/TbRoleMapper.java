6005d81c396a334f137d14c8c94263ce8fb0a89c
Stephanie0224 <715155296@qq.com>
2019-01-21 14:18:22 +0800

    角色管理的增删改查/角色管理的功能 Signed-off-by: Stephanie0224 <715155296@qq.com>

--- src/main/java/cn/com/connext/oms/mapper/TbRoleMapper.java
+++ src/main/java/cn/com/connext/oms/mapper/TbRoleMapper.java
@@ -28,4 +28,56 @@ public interface TbRoleMapper extends MyMapper<TbRole> {
     * @Date: 2019/1/17
     */
     List<TbRole> getListRole();
+
+    /**
+     * @Title:addRoleInfo
+     * @Description:添加
+     * @param  @return
+     * @return int 返回类型
+     * @throws
+     */
+    public int addRole(TbRole tbRole);
+
+    /**
+     * @Title:delRoleInfo
+     * @Description:删除
+     * @param @param
+     * @param @return
+     * @throws
+     */
+    public int delRole(int roleId);
+
+    /**
+     * @Title:updateRole
+     * @Description:修改
+     * @param @param
+     * @param @return
+     * @return int
+     * @throws
+     */
+    public int updateRole(Integer id,String permission_id,String role_id);
+
+    /**
+     * @Title:findRoleInfo
+     * @Description:查询
+     * @param
+     * @return List<TbRole>返回类型
+     * @throws
+     */
+
+    public List<TbRole> findRole();
+
+    /**
+     * @Description:根据role_id查询role_name
+     * @param roleId
+     * @return
+     */
+    TbRole fingRoleNameById(Integer roleId);
+
+    /**
+     * @Description:根据role_id查询role_name
+     * @param roleName
+     * @return
+     */
+    TbRole findIdByRoleName(Integer roleName);
 }
